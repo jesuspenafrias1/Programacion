51eb3ded00e59cce3dab3a14e50c9d7502afc1f0
jesuspenafrias1 <133056790+jesuspenafrias1@users.noreply.github.com>
2023-05-27 13:58:02 -0600

    Add files via upload

---
+++ Unidasda1/ResolverOperaciones.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ordendeoperaciones;
+
+import java.util.Scanner;
+
+public class ResolverOperaciones {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double resultadoA = resolverOperacionA();
+        System.out.println("Resultado a) 3/2 + 4/3 = " + resultadoA);
+
+        double x = obtenerValorVariable("x");
+        double y = obtenerValorVariable("y");
+        double resultadoB = resolverOperacionB(x, y);
+        System.out.println("Resultado b) 1/(x-5) - 3xy/4 = " + resultadoB);
+
+        double resultadoC = resolverOperacionC();
+        System.out.println("Resultado c) 1/2 + 7 = " + resultadoC);
+
+        double resultadoD = resolverOperacionD();
+        System.out.println("Resultado d) 7 + 1/2 = " + resultadoD);
+
+        double a = obtenerValorVariable("a");
+        double b = obtenerValorVariable("b");
+        double c = obtenerValorVariable("c");
+        double d = obtenerValorVariable("d");
+        double e = obtenerValorVariable("e");
+        double f = obtenerValorVariable("f");
+        double g = obtenerValorVariable("g");
+        double h = obtenerValorVariable("h");
+        double j = obtenerValorVariable("j");
+
+        double resultadoE = resolverOperacionE(a, b, c, d, e, f, g, h, j);
+        System.out.println("Resultado e) (a^2/(b-c)) + ((d-e)/(f-(g*h)/j)) = " + resultadoE);
+
+        double m = obtenerValorVariable("m");
+        double n = obtenerValorVariable("n");
+        double p = obtenerValorVariable("p");
+
+        double resultadoF = resolverOperacionF(m, n, p);
+        System.out.println("Resultado f) m/n + p = " + resultadoF);
+
+        double resultadoG = resolverOperacionG(m, n, p);
+        System.out.println("Resultado g) (m+n)/(p-q) = " + resultadoG);
+
+        double resultadoH = resolverOperacionH(a, b, c, d);
+        System.out.println("Resultado h) (a^2/b^2) + (c^2/d^2) = " + resultadoH);
+
+        double q = obtenerValorVariable("q");
+        double r = obtenerValorVariable("r");
+        double s = obtenerValorVariable("s");
+
+        double resultadoI = resolverOperacionI(m, n, p, q, r, s);
+        System.out.println("Resultado i) (m+n)/(q-p/(r/s)) = " + resultadoI);
+
+        double resultadoJ = resolverOperacionJ(a, b, c, d, e, f, g, h);
+        System.out.println("Resultado j) (3a+b)/(c-(d+5e)/(f*g/2h)) = " + resultadoJ);
+
+        double resultadoK = resolverOperacionK(a, b, x);
+        System.out.println("Resultado k) (a^2+2ab+b^2)/(1/x^2 + 2) = " + resultadoK);
+    }
+
+    public static double resolverOperacionA() {
+        double resultado = (3.0 / 2.0) + (4.0 / 3.0);
+        return resultado;
+    }
+
+    public static double resolverOperacionB(double x, double y) {
+        double resultado = 1.0 / (x - 5) - (3 * x * y) / 4;
+        return resultado;
+    }
+
+    public static double resolverOperacionC() {
+        double resultado = 1.0 / 2.0 + 7;
+        return resultado;
+    }
+
+    public static double resolverOperacionD() {
+        double resultado = 7 + 1.0 / 2.0;
+        return resultado;
+    }
+
+    public static double resolverOperacionE(double a, double b, double c, double d, double e, double f, double g, double h, double j) {
+        double resultado = (Math.pow(a, 2) / (b - c)) + ((d - e) / (f - (g * h) / j));
+        return resultado;
+    }
+
+    public static double resolverOperacionF(double m, double n, double p) {
+        double resultado = (m + n) / p;
+        return resultado;
+    }
+
+    public static double resolverOperacionG(double m, double n, double p) {
+        double resultado = (m + n) / (p - obtenerValorVariable("q"));
+        return resultado;
+    }
+
+    public static double resolverOperacionH(double a, double b, double c, double d) {
+        double resultado = (Math.pow(a, 2) / Math.pow(b, 2)) + (Math.pow(c, 2) / Math.pow(d, 2));
+        return resultado;
+    }
+
+    public static double resolverOperacionI(double m, double n, double p, double q, double r, double s) {
+        double resultado = (m + n) / (q - p / (r / s));
+        return resultado;
+    }
+
+    public static double resolverOperacionJ(double a, double b, double c, double d, double e, double f, double g, double h) {
+        double resultado = (3 * a + b) / (c - (d + 5 * e) / (f * g / (2 * h)));
+        return resultado;
+    }
+
+    public static double resolverOperacionK(double a, double b, double x) {
+        double resultado = (Math.pow(a, 2) + 2 * a * b + Math.pow(b, 2)) / (1 / Math.pow(x, 2) + 2);
+        return resultado;
+    }
+
+    public static double obtenerValorVariable(String variable) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el valor de " + variable + ": ");
+        double valor = scanner.nextDouble();
+        return valor;
+    }
+}
+
