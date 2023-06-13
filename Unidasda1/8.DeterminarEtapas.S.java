557dcc19848d36f012fcb85dde9957b172f3aa30
jesuspenafrias1 <133056790+jesuspenafrias1@users.noreply.github.com>
2023-06-13 11:26:14 -0600

    8.DeterminarEtapas.S.java

---
+++ Unidasda1/8.DeterminarEtapas.S.java
@@ -0,0 +1,22 @@
+
+package edades;
+
+ import java.util.Scanner;
+
+public class etapasEdades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa tu edad: ");
+        int edad = scanner.nextInt();
+
+        if (edad <=5 && edad >=1) {
+            System.out.println("Infante");
+        } else if (edad <=17 && edad >=6) {
+            System.out.println("Niño");
+        } else {
+            System.out.println("Adulto");
+        }
+        
+    }
+}
